674837870b79e1e29b21f31fc807f2b6ffd85343
Tao Langston <tao@bway.net>
2016-01-30 12:00:18 -0500

    Periodic Check in

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -1,46 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControl : MonoBehaviour {
+public class EnemyControl : MonoBehaviour {
 
 	public Vector2 speed = new Vector2(50,50);
 	public GameObject shot;
 	public Vector2 shotSpeed;
 	public float shotInterval=1.0f;
-
+	
 	private Vector2 movement;
 	private Rigidbody2D phys;
 	private float nextShot=0;
 
-	public static PlayerControl instance;
-
 	// Use this for initialization
 	void Start () {
 		phys = GetComponent<Rigidbody2D> ();
-		instance = this;
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-	
-		nextShot += Time.deltaTime;
-
-		float inputX = Input.GetAxis ("Horizontal");
-		float inputY = Input.GetAxis ("Vertical");
-
-		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
-
+		
 		Vector2 shotDir = new Vector2(0,0);
-
-		if (Input.GetKey (KeyCode.I)) {
-			shotDir = new Vector2(0,shotSpeed.y);
-		} else if (Input.GetKey (KeyCode.J)) {
-			shotDir = new Vector2(-shotSpeed.x,0);
-		} else if (Input.GetKey (KeyCode.K)) {
-			shotDir = new Vector2(0,-shotSpeed.y);
-		} else if (Input.GetKey (KeyCode.L)) {
-			shotDir = new Vector2(shotSpeed.x,0);
-		}
 
 		if (shotDir != new Vector2(0,0) && nextShot>shotInterval) {
 			nextShot = 0;
@@ -51,11 +31,11 @@ public class PlayerControl : MonoBehaviour {
 			newShot.transform.position = new Vector2(transform.position.x + shotDir.x/shotSpeed.x,transform.position.y+shotDir.y/shotSpeed.y);
 			//SFX
 		}
-
+		
 		// Clamp player to screen
-
+		
 		var dist = (transform.position - Camera.main.transform.position).z;
-
+		
 		var leftBorder = Camera.main.ViewportToWorldPoint(
 			new Vector3(0.1f, 0, dist)
 			).x;
@@ -77,9 +57,15 @@ public class PlayerControl : MonoBehaviour {
 			Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
 			transform.position.z
 			);
-
+		
 		phys.AddForce (movement);
 
-
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.GetComponent<ShotScript> () != null) {
+			Destroy (gameObject);
+		}
 	}
 }
