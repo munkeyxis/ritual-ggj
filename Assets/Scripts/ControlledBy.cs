234286376f8e023009864f3cbe81ad98d106bd09
Chris Stevens <chris.stevens@surveysampling.com>
2016-01-30 10:08:38 -0500

    towns data structure and controlledBy enum

---
+++ Assets/Scripts/ControlledBy.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public enum ControlledBy
+{
+	Player,
+	Neutral,
+	Enemy
+}
+
---
+++ Assets/Scripts/Town.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Town {
+	ControlledBy _controlledBy = ControlledBy.Neutral;
+	Vector2 _position = new Vector2();
+	bool _underAttack = false;
+	int _turnsUntilTaken = 0;
+	bool _playerPresent = false;
+}
+
