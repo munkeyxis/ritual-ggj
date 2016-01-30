c4dbc4af78e17bc8229393f499cd5ce0f8a16011
Chris Stevens <chris.stevens@surveysampling.com>
2016-01-30 11:07:35 -0500

    startup static TownManager in Manager

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(TownManager))]
 public class Manager : MonoBehaviour {
+
+	public static TownManager TownManager {get; private set;}
+
+	private List<IGameManager> _startupList;
+
 	void Awake () {
-	
+		TownManager = GetComponent<TownManager>();
+
+		_startupList = new List<IGameManager>();
+		_startupList.Add(TownManager);
 	}
 }
