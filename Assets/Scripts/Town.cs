991a331b6e66734ffe4d64cb269851c286578ece
Chris Stevens <chris.stevens@surveysampling.com>
2016-01-30 10:36:15 -0500

    add town constructor data structure

--- Assets/Scripts/Town.cs
+++ Assets/Scripts/Town.cs
@@ -3,12 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Town {
-	ControlledBy _controlledBy = ControlledBy.Neutral;
-	Vector2 _position = new Vector2();
+	ControlledBy _controlledBy;
+	Vector2 _position;
 	bool _underAttack = false;
 	int _turnsUntilTaken = 0;
 	bool _playerPresent = false;
 	ElementTypes _elementType;
 	List<Town> _adjacentTowns = new List<Town>();
+
+	public Town(Vector2 position, ControlledBy controlledBy, ElementTypes type, bool playerPresent) {
+		_position = position;
+		_controlledBy = controlledBy;
+		_elementType = type;
+		_playerPresent = playerPresent;
+	}
 }
 
