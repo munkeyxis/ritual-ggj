1c9a9fcda897e711442b4b2f3a01ab4934302952
Chris Stevens <chris.stevens@surveysampling.com>
2016-01-30 12:01:35 -0500

    add prefab variable to TownsController, assign prefab

--- Assets/Scripts/TownsContoller.cs
+++ Assets/Scripts/TownsContoller.cs
@@ -2,14 +2,9 @@
 using System.Collections;
 
 public class TownsContoller : MonoBehaviour {
+	public GameObject TownPrefab;
 
-	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		
 	}
 }
