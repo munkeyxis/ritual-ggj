884e7ea735103ccfa86a84c8f2e303538bcdf58e
Chris Stevens <chris.stevens@surveysampling.com>
2016-01-30 10:49:48 -0500

    add TownManager and startup method

---
+++ Assets/Scripts/TownManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TownManager : MonoBehaviour, IGameManager
+{
+	List<Town> _towns;
+
+	public void StartUp() {
+		// starting player town
+		_towns.Add(new Town(new Vector2(5, -2), ControlledBy.Player, ElementTypes.Fire, true));
+
+		// neatural towns
+		_towns.Add(new Town(new Vector2(2, -4), ControlledBy.Neutral, ElementTypes.Earth, false));
+		_towns.Add(new Town(new Vector2(1, 2), ControlledBy.Neutral, ElementTypes.Earth, false));
+		_towns.Add(new Town(new Vector2(-3, 5), ControlledBy.Neutral, ElementTypes.Earth, false));
+
+		// starting enemy town
+		_towns.Add(new Town(new Vector2(-5, 2), ControlledBy.Enemy, ElementTypes.Water, false));
+	}
+}
+
