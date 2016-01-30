31b2562ae8d4f89d04d2fd9f2802a8e0df8b9fef
Chris Stevens <chris.stevens@surveysampling.com>
2016-01-30 12:12:52 -0500

    display town prefabs

--- Assets/Scripts/TownsContoller.cs
+++ Assets/Scripts/TownsContoller.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TownsContoller : MonoBehaviour {
 	public GameObject TownPrefab;
+	List<Town> _townsDisplayed;
 
 	void Start () {
-		
+		_townsDisplayed = new List<Town> ();
+
+		foreach (Town town in Manager.TownManager._towns) {
+			GameObject townInstance = Instantiate (TownPrefab);
+			townInstance.transform.SetParent (this.transform);
+			townInstance.transform.position = town._position;
+		}
 	}
 }
